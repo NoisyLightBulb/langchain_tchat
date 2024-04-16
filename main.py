a91be1273bc5cc0dcc30ee99fbd5244c469c42b7
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-04-16 17:30:41 +0200

    storing chat history accross sessions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 #import environment variables
@@ -11,7 +11,11 @@ load_dotenv()
 chat = ChatOpenAI()
 
 #initialize chat memory
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationBufferMemory(
+    chat_memory = FileChatMessageHistory("messages.json"),          #storing message history across sessions
+    memory_key = "messages",
+    return_messages = True
+    )
 
 #chat prompt template
 prompt = ChatPromptTemplate(
